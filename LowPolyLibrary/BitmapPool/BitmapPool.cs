00657a39df3983a1ec68e38df18b8142ef5264f0
Cameron White <cameronwhite08@gmail.com>
2017-05-17 22:59:25 -0400

    Included classes to implement a bitmap pool that will manage bitmap memory

---
+++ LowPolyLibrary/BitmapPool/BitmapPool.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.Graphics;
+
+namespace LowPolyLibrary.BitmapPool
+{
+	/**
+    * A pool of fixed-size Bitmaps. Leases a managed Bitmap object
+    * which is tied to this pool. Bitmaps are put back to the pool
+    * instead of actual recycling.
+    *
+    * WARNING: This class is NOT thread safe, intended for use
+    *          from the main thread only.
+    */
+    public class BitmapPool
+    {
+		private readonly int _width;
+		private readonly int _height;
+		private readonly Bitmap.Config _config;
+		private readonly Stack<Bitmap> _bitmaps = new Stack<Bitmap>();
+		private bool _isRecycled;
+
+		//private final Handler handler = new Handler();
+
+		public BitmapPool(int bitmapWidth, int bitmapHeight, Bitmap.Config conf)
+        {
+            _width = bitmapWidth;
+            _height = bitmapHeight;
+            _config = conf;
+        }
+
+		/**
+        * Destroy the pool. Any leased IManagedBitmap items remain valid
+        * until they are recycled.
+        * */
+        public void recycle()
+        {
+            _isRecycled = true;
+            foreach (var bitmap in _bitmaps)
+            {
+                bitmap.Recycle();
+            }
+            _bitmaps.Clear();
+        }
+
+		/**
+        * Get a Bitmap from the pool or create a new one.
+        * @return a managed Bitmap tied to this pool
+        */
+        public IManagedBitmap getBitmap()
+        {
+            return new LeasedBitmap(_bitmaps.Count == 0 ? Bitmap.CreateBitmap(_width, _height, _config) : _bitmaps.Pop(), this);
+		}
+
+        public class LeasedBitmap : IManagedBitmap
+        {
+            private BitmapPool _pool;
+
+            private int referenceCounter = 1;
+            private readonly Bitmap _bitmap;
+
+            internal LeasedBitmap(Bitmap bitmap, BitmapPool pool)
+            {
+                _bitmap = bitmap;
+                _pool = pool;
+            }
+
+            public Bitmap GetBitmap()
+            {
+                return _bitmap;
+            }
+
+            public void recycle()
+            {
+				if (--referenceCounter == 0)
+				{
+					if (_pool._isRecycled)
+					{
+						_bitmap.Recycle();
+					}
+					else
+					{
+						_pool._bitmaps.Push(_bitmap);
+					}
+				}
+            }
+
+            public IManagedBitmap retain()
+            {
+                ++referenceCounter;
+                return this;
+            }
+        }
+    }
+}
---
+++ LowPolyLibrary/BitmapPool/IManagedBitmap.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Android.Graphics;
+
+namespace LowPolyLibrary.BitmapPool
+{
+
+	/**
+    * A reference-counted Bitmap object. The Bitmap is not really recycled
+    * until the reference counter drops to zero.
+    */
+    public interface IManagedBitmap
+    {
+		/*
+        * Get the underlying Bitmap object.
+        * NEVER call Bitmap.recycle() on this object.
+        */
+		Bitmap GetBitmap();
+
+		/**
+        * Decrease the reference counter and recycle the underlying Bitmap
+        * if there are no more references.
+        */
+        void recycle();
+
+		/**
+        * Increase the reference counter.
+        * @return self
+        */
+        IManagedBitmap retain();
+
+    }
+}
